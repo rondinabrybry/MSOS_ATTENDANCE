d134de0360b8d04f05a9762fc0ccb087f8497e30
backend.dude <71830788+rondinabrybry@users.noreply.github.com>
2024-11-21 23:15:07 +0800

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
 
         <div class="max-w-7xl p-12 mx-auto">
-        <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl dark:border-gray-700 dark:bg-gray-800">
             <img class="object-cover w-1/2 rounded-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="{{ asset('storage/' . Auth::user()->profile_image) }}" alt="">
             <div class="flex flex-col justify-between p-4 leading-normal">
                 <h1 class="mb-2 text-6xl font-bold tracking-tight text-gray-900 dark:text-white">{{ Auth::user()->name }}</h1>
