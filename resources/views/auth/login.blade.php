76d0cc20f45e758582badef6b7a78622c7118da2
backend.dude <71830788+rondinabrybry@users.noreply.github.com>
2024-11-21 17:29:14 +0800

    disable register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,12 +39,12 @@
             </label>
         </div>
         <div class="flex items-center justify-end mt-4 mb-4">
-            @if (Route::has('register'))
+            {{-- @if (Route::has('register'))
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                 href="{{ route('register') }}">
                 {{ __('Register Here') }}
             </a>
-            @endif
+            @endif --}}
 
 
             <x-primary-button class="ms-3">
